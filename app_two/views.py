831e56c73feb9cd296589002f0df18d3d869577b
ChristinaGaitan <christina.gaitan@gmail.com>
2020-07-14 16:49:13 -0500

    Modify template styles

--- app_two/views.py
+++ app_two/views.py
@@ -4,7 +4,8 @@ from app_two.models import User
 
 # Create your views here.
 def index(request):
-  return HttpResponse('<h1>Welcome!</h1><p>Go to /users to see the list of user information!</p>')
+  return render(request, 'app_two/index.html')
+  # return HttpResponse('<h1>Welcome!</h1><p>Go to /users to see the list of user information!</p>')
 
 def help(request):
   help_dict = { 'help_insert': 'HELP PAGE' }
